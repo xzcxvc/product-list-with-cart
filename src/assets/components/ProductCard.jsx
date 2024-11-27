b8ba17935aea963657c1a7ef269ae4a62447481a
xzcxvc <daryl.tiamps@gmail.com>
2024-11-28 02:15:24 +0800

    frontend changes

--- src/assets/components/ProductCard.jsx
+++ src/assets/components/ProductCard.jsx
@@ -5,23 +5,11 @@ const ProductCard = ({ id, img, title, desc, price }) => {
     <div className="product-card flex flex-col gap-0 w-full" key={id}>
       <div className="product-img">
         <div className="relative">
-          <img src={img} alt="" className="rounded-lg w-full" />
+          <img src={img} alt="" className="rounded-lg w-full shadow-xl" />
           <AddToCartButton />
         </div>
-
-        {/* {img ? (
-          <div>
-            {img}
-            <img src={img} alt="" className="rounded-lg relative" />
-            <AddToCartButton />
-          </div>
-        ) : (
-          <>
-            <div className="h-[400px] bg-gradient-to-r from-slate-500 to-slate-300 bg-[length:200%_200%] animate-gradientMove rounded-xl "></div>
-            <AddToCartButton />
-          </>
-        )} */}
       </div>
+      
       <div className="product-details font-redhatdisplay">
         <p className="text-[14px] text-gray-500">{title ? title : "Title"}</p>
         <p className="font-bold">{desc ? desc : "Description"}</p>
